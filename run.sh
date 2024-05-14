631b63d42308fe1fa7231801637b9f1b047ad6ad
arthur sousa <arthursvrr@gmail.com>
2024-05-14 17:34:41 -0300

    largge model results

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
- CUDA_VISIBLE_DEVICES=0 python run.py configs/instant-mesh-large.yaml examples/plant.jpg --save_video --view 6 --seed 42 --diffusion_steps 100
+ CUDA_VISIBLE_DEVICES=0 python run.py configs/instant-mesh-large.yaml examples/pikachu.png --save_video --view 6 --seed 3407 --diffusion_steps 100
