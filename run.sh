fa11317f5e349186bb322734b0bcad7f040f17ed
arthur sousa <arthursvrr@gmail.com>
2024-05-14 17:20:03 -0300

    more example results

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
- CUDA_VISIBLE_DEVICES=0 python run.py configs/instant-mesh-base.yaml examples/fruit_elephant.jpg --save_video --view 6 --seed 42 --diffusion_steps 100
+ CUDA_VISIBLE_DEVICES=0 python run.py configs/instant-mesh-large.yaml examples/plant.jpg --save_video --view 6 --seed 42 --diffusion_steps 100
